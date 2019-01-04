863cc958343e1b4465249c71bde6fe1ae49efb63
Matthew Tekampe <TekampML19@pwcs-edu.org>
2019-01-04 13:32:24 -0500

    colton u smallesst

--- src/firstTry/avg.java
+++ src/firstTry/avg.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class avg {
 	public static void main(String[] args)
 	{
-		System.out.println("Eter 2 numers");
+		System.out.println("Enter 2 numbers");
 		Scanner in = new Scanner(System.in);
 		
 		int one = in.nextInt();
 		int two = in.nextInt();
-		int ans = (one * two) + 2;
+		int ans = (one + two) / 2;
 		System.out.print("\nYour average is " + ans);
 	}
 
